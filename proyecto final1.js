0cd5e18b10f906e658988f9fb07739758b1009c9
Mati240594 <80931005+Mati240594@users.noreply.github.com>
2021-04-13 21:41:48 -0300

    Add files via upload

---
+++ proyecto final1.js
@@ -0,0 +1,54 @@
+function MO1(){
+var MO1=prompt("El microorganismo que deseo buscar es");
+if((MO1=="Escherichia coli" ) ||(MO1=="coli") || (MO1==escherichia)){
+
+
+class Microorganismo1{
+    constructor(Nombre,clasificación,informacion,imagen){
+    this.Nombre= "Escherichia Coli";
+    this.clasificación="Es miembro de la familia enterobacteria y estas afectan al tracto gastrointestinal";
+    this.informacion="Informacion"; //Acá entraria la información que conocida de la bacteria no lo puse para 
+    //no llenar el código de información que no tiene sentido evaluar ahora.
+    this.imagen= "Link o Imagen/video a cargar";
+}
+}
+} else{
+    alert("No existe Microorganismo con ese nombre");
+}}
+function MO2(){
+    var MO2=prompt("El microorganismo que deseo buscar es");
+    if((MO2=="Salmonella" ) ||(MO2=="salmonella") ){
+    
+    
+    class Microorganismo1{
+        constructor(Nombre,clasificación,informacion,imagen){
+        this.Nombre= "Salmonella";
+        this.clasificación="Pertenecen a la familia Enterobacteriaceae";
+        this.informacion="Informacion"; //Acá entraria la información 
+        this.imagen= "Link o Imagen/video a cargar";
+    }
+    }
+    } else{
+        alert("No existe Microorganismo con ese nombre");
+    }}
+    function MO3(){
+        var MO3=prompt("El microorganismo que deseo buscar es");
+        if((MO3=="Helicobacter pylori" ) ||(MO3=="helicobacter")||(MO3=="pylori") ){
+        
+        
+        class Microorganismo1{
+            constructor(Nombre,clasificación,informacion,imagen){
+            this.Nombre= "Helicobacter pylori";
+            this.clasificación="Pertenecen a la familia Proteobacteria";
+            this.informacion="Informacion"; //Acá entraria la información 
+            this.imagen= "Link o Imagen/video a cargar";
+        }
+        }
+        } else{
+            alert("No existe Microorganismo con ese nombre");
+        }}
+MO1();
+MO2();
+MO3();
+
+
